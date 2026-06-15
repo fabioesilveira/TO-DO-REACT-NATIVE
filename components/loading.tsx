ce111379abafeb7c6f00b6d52e45d4e6d2aa8e09
Fabio Silveira <fabio.eds@hotmail.com>
2026-06-15 11:58:25 -0700

    cleaned files and assets not used

--- components/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { ActivityIndicator, View } from 'react-native';
-
-export default function Loading() {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <ActivityIndicator size="large" color="#007bff" />
-        </View>
-    );
-}
